04b8acd396488694e6ce1de0330e23589f47a141
Shivanand potle <shpotle_it@jspmrscoe.edu.in>
2026-01-12 13:03:36 +0530

    Fix CORS origin URL in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ new Razorpay({
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-app.use(cors({ origin: 'http://localhost:8080' || 'https://sambhav-official-frontend.vercel.app/', credentials: true }));
+app.use(cors({ origin: 'http://localhost:8080' || 'https://sambhav-frontend.onrender.com', credentials: true }));
 app.use(express.json());
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -151,4 +151,5 @@ app.get('/api/registrations', requireAdminLogin, async (req, res) => {
 connectToDatabase().then(() => {
   app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
 });
+
 
